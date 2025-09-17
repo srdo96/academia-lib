8a55f4cff86aca1c874be3f3c413a58689cf4e91
Sakib Rahman <sue.sakib@gmail.com>
2025-09-17 21:59:40 +0600

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import localFont from "next/font/local";
+
+const ibmPlexSans = localFont({
+  src: [
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    {
+      path: "./fonts/IBMPlexSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "/fonts/IBMPlexSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
+});
+
+const babasNeue = localFont({
+  src: [
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: "--bebas-neue",
+});
+
+export const metadata: Metadata = {
+  title: "Academia lib",
+  description: "Academia Lib is a University library management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexSans.className} ${babasNeue.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
